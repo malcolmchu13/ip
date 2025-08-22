54b9bf32290f07ca8f51f28bd49e13ec263ea33b
malcolmchu13 <malcolmchu@Malcolms-MacBook-Pro.local>
2025-08-22 17:11:40 +1000

    Level-0

---
+++ src/main/java/rat.java
@@ -0,0 +1,7 @@
+public class rat {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm rat\n " + "What can I do for you? \n\n"
+        + "Bye. Hope to see you again soon!");
+    }
+}
