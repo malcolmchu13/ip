0168d4c1c9ead164b7e78123264f8b6ee1438ebb
malcolmchu13 <malcolmchu@Malcolms-MacBook-Pro.local>
2025-08-22 17:15:34 +1000

    Level-1

--- src/main/java/rat.java
+++ src/main/java/rat.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class rat {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("""
                 Hello! I'm rat
                  What can I do for you?\s
                 
                 Bye. Hope to see you again soon!""");
+        while (true) {
+            String input = scanner.nextLine();   // read user command
+            if (input.equals("bye")) {
+                break;   // exit loop if user types "bye"
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);     // echo the command
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
